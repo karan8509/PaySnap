bd325bd9b392dc37d3c0ec64b519df56071d0182
karan8509 <kj382542@gmail.com>
2025-05-04 20:16:58 +0330

    QR code generator UI with amount input

---
+++ ui/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import {QRCodeGenerator} from "./component/QRCodeGenerator";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <QRCodeGenerator />
+    </>
+  );
+}
+
+export default App;
---
+++ ui/src/component/QRCodeGenerator.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Qrcode from "react-qr-code";
+export const QRCodeGenerator = () => {
+  const [number, setNumber] = useState("");
+
+  const handleClick = () => {
+    <h1>{number && <Qrcode value={number} />}</h1>
+    
+  };
+  return (
+    <div>
+      <h1>QRCode Generator</h1>
+      <div className="container">
+        <input
+          type="number"
+          name="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button onClick={handleClick}>Generator code </button>
+     
+      </div>
+    </div>
+  );
+};
